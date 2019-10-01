dbeddb79b5b5c211bd125bfa6457017bf46d8e15
SepidehN <s.nazemi10@gmail.com>
2019-10-01 14:49:34 +0100

    here is the initial step of the capstone project which includes creating file and commit the code to Github.

---
+++ CapstoneProject.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Applied Data Science
+# ### The capstone project 
+# #### This notebook will be mainly used for the capstone project
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
